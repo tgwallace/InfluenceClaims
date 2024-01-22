06c53dc7a7e7ac34b1d8ab3ffc7e5203c7f384d2
BuildTools <unconfigured@null.spigotmc.org>
2024-01-22 17:02:07 -0500

    Updated Special Elections to switch nobility -> roles system.

--- src/main/java/hardbuckaroo/influenceclaims/city/elections/CitySpecialElection.java
+++ src/main/java/hardbuckaroo/influenceclaims/city/elections/CitySpecialElection.java
@@ -13,9 +13,7 @@ import org.bukkit.event.Listener;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class CitySpecialElection implements CommandExecutor, Listener {
     private final InfluenceClaims plugin;
@@ -46,22 +44,37 @@ public class CitySpecialElection implements CommandExecutor, Listener {
             return true;
         }
 
+        if(cityData.contains(cityUUID+".Elections.Overthrow")) {
+            player.sendRawMessage("Cannot call for a special election while an overthrow is underway.");
+            return true;
+        }
+
         if(cityData.getBoolean(cityUUID+".SpecialElectionCooldown")) {
             player.sendRawMessage("Cannot call for a special election when one has already been called this term.");
             return true;
         }
 
         String government = cityData.getString(cityUUID+".Government");
-        List<String> nobles = cityData.getStringList(cityUUID+".Nobles");
+
+        List<String> nobles = new ArrayList<>();
+        if(cityData.contains(cityUUID+".Roles")) {
+            for (String title : cityData.getConfigurationSection(cityUUID + ".Roles").getKeys(false)) {
+                if(cityData.getBoolean(cityUUID+".Roles."+title+".Permissions.Vote")) {
+                    nobles.addAll(cityData.getStringList(cityUUID + ".Roles." + title + ".Players"));
+                }
+            }
+            Set<String> dedupe = new LinkedHashSet<>(nobles);
+            nobles.clear();
+            nobles.addAll(dedupe);
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String leaderTitle = cityData.getString(cityUUID+".LeaderTitle");
-        String nobilityTitle = cityData.getString(cityUUID+".NobilityTitle");
 
         if(Objects.requireNonNull(government).equalsIgnoreCase("Monarchy")) {
             player.sendRawMessage("Cannot call a special election in a Monarchy. Use /CityOverthrow if you wish to call a vote to overthrow the current " + leaderTitle + "!");
             return true;
         } else if (Objects.requireNonNull(government).equalsIgnoreCase("Oligarchy") && !nobles.contains(playerUUID)) {
-            player.sendRawMessage("Only a " + nobilityTitle + " can call a special election in an Oligarchy! Use /CityOverthrow if you wish to call a vote to overthrow the current government.");
+            player.sendRawMessage("Only those with voting rights can call a special election in an Oligarchy! Use /CityOverthrow if you wish to call a vote to overthrow the current government.");
             return true;
         }
 
@@ -81,7 +94,7 @@ public class CitySpecialElection implements CommandExecutor, Listener {
             cityData.set(cityUUID+".Elections.SpecialElection.VoteCount.Yes",0);
             cityData.set(cityUUID+".Elections.SpecialElection.VoteCount.No",0);
             cityData.set(cityUUID+".Elections.SpecialElection.StartDate",LocalDate.now().format(formatter));
-            plugin.cityMessage(cityUUID, "A vote for a special election has been called! If you are a " + cityData.getString(cityUUID+".NobilityTitle") + ", use /CityVote to vote.",true);
+            plugin.cityMessage(cityUUID, "A vote for a special election has been called! If you are eligible, use /CityVote to vote.",true);
         } else if (government.equalsIgnoreCase("Democracy") && strings.length >= 1 && strings[0].equalsIgnoreCase("confirm")) {
             cityData.set(cityUUID+".Elections.SpecialElection.VoteCount.Yes",0);
             cityData.set(cityUUID+".Elections.SpecialElection.VoteCount.No",0);
