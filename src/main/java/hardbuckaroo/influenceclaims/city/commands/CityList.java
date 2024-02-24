be9abc68fbc2839f2bcc2644d93227772fdf3251
BuildTools <unconfigured@null.spigotmc.org>
2024-02-24 15:58:08 -0500

    Fixed CityList next line addition.

--- src/main/java/hardbuckaroo/influenceclaims/city/commands/CityList.java
+++ src/main/java/hardbuckaroo/influenceclaims/city/commands/CityList.java
@@ -45,22 +45,21 @@ public class CityList implements CommandExecutor {
             map.entrySet().stream().sorted(Map.Entry.<String,Long>comparingByValue().reversed()).forEachOrdered(x -> mapSorted.put(x.getKey(), x.getValue()));
 
             int count = 0;
-            boolean lastPage = false;
             for(Map.Entry<String, Long> entry : mapSorted.entrySet()) {
+                if(count>=(page+1)*5) {
+                    TextComponent nextLine = new TextComponent(plugin.color("\n&oNext Page &r->"));
+                    nextLine.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/citylist " + (page + 1)));
+                    message.addExtra(nextLine);
+                    break;
+                }
                 if(count>=page*5 && count<(page+1)*5) {
                     String cityUUID = entry.getKey();
                     long influence = entry.getValue();
                     TextComponent subComponent = new TextComponent(plugin.color("\n&l" + cityData.getString(cityUUID + ".Color") + cityData.getString(cityUUID + ".Name") + " &r(" + NumberFormat.getInstance().format(influence) + "): &o" + cityData.getString(cityUUID + ".Motto")));
                     subComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/cityinfo " + cityData.getString(cityUUID + ".Name")));
                     message.addExtra(subComponent);
-                    lastPage = count % 5 != 0;
                 }
                 count++;
-            }
-            if(!lastPage) {
-                TextComponent nextLine = new TextComponent(plugin.color("\n&oNext Page &r->"));
-                nextLine.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/citylist " + (page + 1)));
-                message.addExtra(nextLine);
             }
             player.spigot().sendMessage(message);
         }
